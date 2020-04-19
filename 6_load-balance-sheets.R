98e592a5befe6c064b544af208444053ee731d5e
Petr Bouchal <pbouchal@gmail.com>
2020-04-19 13:44:59 +0200

    load, process and save balance sheet data

---
+++ 6_load-balance-sheets.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(statnipokladna)
+
+source("shared.R")
+
+rozvaha_1 <- statnipokladna::sp_get_table("balance-sheet",
+                                              year = 2010:2019,
+                                              ico = ico_obce,
+                                              dest_dir = "data-input")
+
+rozvaha_1_processed <- rozvaha_1 %>%
+  sp_add_codelist("polvyk") %>%
+  sp_add_codelist(orgs, by = "ico")
+
+rozvaha_2 <- statnipokladna::sp_get_table("balance-sheet-2",
+                                            year = 2013:2019,
+                                            ico = ico_obce, dest_dir = "data-input")
+
+rozvaha_2 %>%
+  group_by(per_yr) %>%
+  summarise(n_2 = n_distinct(ico)) %>%
+  right_join(rozvaha_1 %>%
+              group_by(per_yr) %>%
+              summarise(n_1 = n_distinct(ico))) %>%
+  replace_na(list(n_2 = 0)) %>%
+  mutate(n = n_1 + n_2)
+
+rozvaha_2_processed <- rozvaha_2 %>%
+  sp_add_codelist("polvyk") %>%
+  sp_add_codelist(orgs, by = "ico")
+
+rozvaha_processed <- bind_rows(rozvaha_1_processed, rozvaha_2_processed) %>%
+  filter(polvyk %in% rozvaha_keep | synuc %in% synuc_dluh) %>%
+  sp_add_codelist("katobyv")
+
+rozvaha_processed_unfiltered <- bind_rows(rozvaha_1_processed, rozvaha_2_processed) %>%
+  sp_add_codelist("katobyv")
+
+write_rds(rozvaha_processed, "data-processed/rozvaha_filtered.rds", compress = "gz")
+write_rds(rozvaha_processed_unfiltered, "data-processed/rozvaha.rds", compress = "gz")
