2357a11623651137ae37b3f908e377bbe46f879d
Petr Bouchal <pbouchal@gmail.com>
2020-04-19 13:43:10 +0200

    document

--- 2_prep-codelists.R
+++ 2_prep-codelists.R
@@ -3,12 +3,15 @@ polozka <- polozka_raw %>%
   # fix codes where end date is before start date
   mutate(end_date = if_else(polozka %in% c("1381", "2326") & end_date == "2018-09-30",
                             as.Date("9999-12-31"), end_date),
+  # detect codes in problematic 2018 periods
+         for2018 = (end_date >=
                       "2018-12-31" & start_date <= "2018-12-31")) %>%
   group_by(polozka, for2018) %>%
   mutate(n = n(),
          duplicate = n() > 1,
          latest = end_date == max(end_date)) %>%
   # View()
+  # filter duplicates out
   filter(!(for2018 & duplicate & latest)) %>%
   ungroup() %>%
   select(-for2018, -duplicate, -latest, -n)
