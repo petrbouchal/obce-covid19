adc3a2baafe5ac886f6f2eb3a4f00379717d2d3c
Petr Bouchal <pbouchal@gmail.com>
2020-06-16 00:02:22 +0200

    Add social sharing metadata, closes #16

--- twitter-social-charts.R
+++ twitter-social-charts.R
@@ -12,6 +12,10 @@ outcome_stub = "bilance"
 plot_load("typ-bar")
 ggsave("charts-output/twi-bilance-typ-bar.png", width = 6, height = 4, dpi = "retina", scale = 1)
 
+outcome_stub = "bilance"
+plot_load("typ-bar")
+ggsave("docs/metaimage.png", width = 6, height = 3, dpi = "retina", scale = 1)
+
 outcome_stub = "rezervy"
 plot_load("vel-bar-nula")
 ggsave("charts-output/twi-rezervy-vel-bar.png", width = 6, height = 4, dpi = "retina", scale = 1.5)
@@ -19,3 +23,4 @@ ggsave("charts-output/twi-rezervy-vel-bar.png", width = 6, height = 4, dpi = "re
 outcome_stub = "dluh"
 plot_load("typ-bar-breach")
 ggsave("charts-output/twi-dluh-typ.png", width = 6, height = 4, dpi = "retina", scale = 1)
+
