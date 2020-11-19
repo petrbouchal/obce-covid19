c598a6c8a2452319a61bcfa949e199fc1f69414d
Petr Bouchal <pbouchal@gmail.com>
2020-11-19 19:30:36 +0100

    more exploration code

--- Y_kratk_fin_maj_explore.R
+++ Y_kratk_fin_maj_explore.R
@@ -1,11 +1,12 @@
 indikatory %>%
+  ungroup() %>%
   filter(per_yr %in% 2019:2022) %>%
   # filter(ico == "00064581") %>%
   group_by(ico) %>%
   filter(min(kratkodoby_fin_majetek) == 0) %>%
   select(per_yr, scenario, bilance, bilance_rel, dluh,
          kratkodoby_fin_majetek, cash_drain_to_sum) %>%
-  arrange(ico, scenario, per_yr) %>% View()
+  arrange(ico, scenario, per_yr)
 
 indikatory %>%
   filter(per_yr > 2017) %>%
@@ -15,10 +16,31 @@ indikatory %>%
 
 indikatory %>%
   filter(per_yr %in% 2019:2020) %>%
+  filter(scenario %in% sc_real | per_yr == 2019) %>%
   group_by(per_yr, scenario, scenario_name, dph_change, rud_change, borrowing_ratio,
-           kraj) %>%
-  summarise(n = sum(kratkodoby_fin_majetek == 0)/n()) %>%
+           typobce) %>%
+  summarise(n = sum(kratkodoby_fin_majetek == 0, na.rm = T)/n()) %>%
   View()
+
+indikatory %>%
+  filter(per_yr %in% 2019:2020) %>%
+  filter(scenario %in% sc_real | per_yr == 2019) %>%
+  group_by(per_yr, scenario, scenario_name, dph_change, rud_change, borrowing_ratio,
+           typobce) %>%
+  summarise(n = sum(kratkodoby_fin_majetek < kratkodobe_zavazky, na.rm = T)/n()) %>%
+  View()
+
+indikatory %>%
+  filter(per_yr %in% 2019:2020, dph_change == rud_change) %>%
+  filter(rud_change %in% c(0.8, 1)) %>%
+  drop_na() %>%
+  group_by(per_yr, scenario, scenario_name, rud_change, borrowing_ratio,
+           typobce
+           ) %>%
+  summarise(n = sum(kratkodoby_fin_majetek == 0)/n()) %>%
+  ggplot(aes(n, as.factor(typobce))) +
+  geom_col() +
+  facet_grid(rud_change ~ borrowing_ratio)
 
 indikatory %>%
   filter(per_yr %in% 2019:2020, dph_change == rud_change) %>%
@@ -26,7 +48,7 @@ indikatory %>%
   group_by(per_yr, scenario, scenario_name, rud_change, borrowing_ratio,
            typobce
            ) %>%
-  summarise(n = sum(kratkodoby_fin_majetek == 0)/n()) %>%
+  summarise(n = sum(kratkodoby_fin_majetek < kratkodobe_zavazky)/n()) %>%
   ggplot(aes(n, as.factor(typobce))) +
   geom_col() +
   facet_grid(rud_change ~ borrowing_ratio)
