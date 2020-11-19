dba13ac41c867a589910fe50fa509fe43e03b2cd
Petr Bouchal <pbouchal@gmail.com>
2020-11-19 19:02:26 +0100

    add summary and plots to index

---
+++ inspect-chart-files.R
@@ -0,0 +1,8 @@
+read_rds("charts-storage/real_prijmy_typ-bar.rds")
+read_rds("charts-storage/real_bilance_typ-bar.rds")
+read_rds("charts-storage/real_prijmy_kraj-bar.rds")
+read_rds("charts-storage/real_bilance_kraj-bar.rds")
+read_rds("charts-storage/pes_prijmy_kraj-bar.rds")
+read_rds("charts-storage/pes_bilance_kraj-bar.rds")
+
+read_rds("charts-storage/real_bilance_typ-hist.rds")
